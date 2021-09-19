4e49171b8008a89e12c3c0597bdbb7ed789954c7
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2021-09-19 07:58:22 -0300

    start page MyAccount

--- src/Pages/MyAccount/index.js
+++ src/Pages/MyAccount/index.js
@@ -10,14 +10,40 @@ export default function MyAccount(){
             <PageArea>
                 <div className='datas'>
                     <form>
-                        <input type='text' name='name' readOnly/>
-                        <input type='email' name='email' readOnly/>
-                        <input type='text' name='state' readOnly/>
-                        <button>Editar</button>
+                        <label className='area'>
+                            <div className='area-titulo'>Nome</div>
+                            <div className='area-input'>
+                                <input type='text' name='name' readOnly/>
+                            </div>
+                        </label>
+                        <label className='area'>
+                            <div className='area-titulo'>E-mail</div>
+                            <div className='area-input'>
+                                <input type='email' name='email' readOnly/>
+                            </div>
+                        </label>
+                        <label className='area'>
+                            <div className='area-titulo'>Estado</div>
+                            <div className='area-input'>
+                                <select className='select' name='state' readOnly>
+                                    <option></option>
+                                </select>
+                            </div>
+                        </label>
+                        <div className='area-input'>
+                            <button>Editar</button>
+                        </div>
                     </form>
                 </div>
                 <div className='posts'>
-
+                    <PageContainer>
+                        <PageArea>
+                            <h2>Seus anúncios</h2>
+                            <div className='list'>
+                                
+                            </div>
+                        </PageArea>
+                    </PageContainer>
                 </div>
             </PageArea>
         </PageContainer>
